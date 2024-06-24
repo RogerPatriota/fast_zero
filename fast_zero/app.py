325a0fe055ae6bfcf4d794782154a51fe713e50c
Roger <rogerpatriota@gmail.com>
2024-06-24 16:35:06 -0300

    add POST endpoint and unit testing

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,28 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
+
+database = []
 
 app = FastAPI()
 
 
-@app.get('/', response_model=Message)
+@app.get('/', response_model=Message, status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Hello World!!'}
 
+
+@app.get('/users/', response_model=UserList, status_code=HTTPStatus.OK)
+def read_users():
+    return {'users': database}
+
+
+@app.post('/users/', response_model=UserPublic, status_code=HTTPStatus.CREATED)
+def create_user(user: UserSchema):
+    user_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_id)
+
+    return user_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,51 @@
 from http import HTTPStatus
 
+import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
 
 
-def test_root_app():
-    client = TestClient(app)  # arrange - Set up the object to be tested
+@pytest.fixture()
+def client():
+    return TestClient(app)  # arrange - Set up the object to be tested
 
-    response = client.get('/')  # act - Act on the object
 
-    # assert - Making declarations about the object (status)
+def test_root_app(client):
+    response = client.get('/')  # act - Act on the object (simulate a request)
+
+    # assert - Making declarations(what you expected) about the object (status)
     assert response.status_code == HTTPStatus.OK
     # assert - Making declarations about the object (response)
     assert response.json() == {'message': 'Hello World!!'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',  # url
+        json={  # body
+            'username': 'Roger',
+            'email': 'roger.santos@gmail.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'Roger',
+        'email': 'roger.santos@gmail.com',
+        'id': 1,
+    }
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'Roger',
+                'email': 'roger.santos@gmail.com',
+                'id': 1,
+            }
+        ]
+    }
