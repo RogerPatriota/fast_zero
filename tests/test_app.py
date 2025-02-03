32fa06f81580b26ae8e1d41a602725e4f1fc540b
roger <rogerpatriota@gmail.com>
2025-02-03 12:29:11 -0300

    test: just a chill test

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,4 +7,4 @@ def test_root_app(client):
     # assert - Making declarations(what you expected) about the object (status)
     assert response.status_code == HTTPStatus.OK
     # assert - Making declarations about the object (response)
-    assert response.json() == {'message': 'Hello World!!'}
+    assert response.json() == {'message': 'Hello World!!!'}
